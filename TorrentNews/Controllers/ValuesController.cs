b837863be4fc6ec479192f3e41f5e734ef4e9872
thefluxcapacitor <thefluxcapacitor@thefluxcapacitor.com>
2013-08-29 21:06:49 -0300

    testing out mongodb and csquery

--- TorrentNews/Controllers/ValuesController.cs
+++ TorrentNews/Controllers/ValuesController.cs
@@ -1,39 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
-
-namespace TorrentNews.Controllers
+﻿namespace TorrentNews.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Web.Http;
+
+    using CsQuery;
+
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization;
+    using MongoDB.Bson.Serialization.Attributes;
+    using MongoDB.Bson.Serialization.IdGenerators;
+    using MongoDB.Driver;
+
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
-        }
+            if (!BsonClassMap.IsClassMapRegistered(typeof(Torrent)))
+            {
+                BsonClassMap.RegisterClassMap<Torrent>();
+            }
 
-        // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+            const int MinSeeds = 20;
+            var page = 1;
+            var client = new HttpClient(new HttpClientHandler
+                {
+                    AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+                });
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+            var response = client.GetAsync(string.Format(
+                "http://kickass.to/usearch/category%3Amovies%20seeds%3A{0}/{1}/?field=time_add&sorder=desc", 
+                MinSeeds, 
+                page)).Result;
+            CQ document = response.Content.ReadAsStringAsync().Result;
+            var rows = document[".data tr"];
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+            MongoClient mc = new MongoClient("mongodb://127.0.0.1:27017");
+            var ms = mc.GetServer();
+            var mdb = ms.GetDatabase("torrentnews");
+            if (!mdb.CollectionExists("torrents"))
+            {
+                mdb.CreateCollection("torrents");
+            }
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
+            var torrents = mdb.GetCollection<Torrent>("torrents");
+            var x = torrents.FindAll().ToList();
+            foreach (var row in rows)
+            {
+                if (row.Cq().HasClass("firstr"))
+                {
+                    continue;
+                }
+
+                var mres = torrents.Insert(new Torrent
+                                               {
+                                                   Title = row.Cq().Find("div.torrentname > a.normalgrey").Text()
+                                               });
+            }
+
+            return rows.Text();
         }
+    }
+
+    public class Torrent
+    {
+        [BsonId(IdGenerator = typeof(ObjectIdGenerator))]
+        public ObjectId Id { get; set; }
+
+        public string Title { get; set; }
     }
 }
