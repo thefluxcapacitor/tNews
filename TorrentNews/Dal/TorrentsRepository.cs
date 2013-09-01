6765429218a673ceb2a50e1b443657523e9615dc
thefluxcapacitor <thefluxcapacitor@thefluxcapacitor.com>
2013-09-01 14:52:45 -0300

    connection settings updated

--- TorrentNews/Dal/TorrentsRepository.cs
+++ TorrentNews/Dal/TorrentsRepository.cs
@@ -15,7 +15,11 @@
 
         public TorrentsRepository()
         {
-            var url = new MongoUrl(ConfigurationManager.AppSettings["MONGOLAB_URI"]);
+            var url = new MongoUrl(
+                ConfigurationManager.AppSettings["MONGOHQ_URL"] ?? 
+                ConfigurationManager.AppSettings["MONGOLAB_URI"] ?? 
+                "mongodb://127.0.0.1:27017/torrentnews");
+
             var mc = new MongoClient(url);
             var ms = mc.GetServer();
             var mdb = ms.GetDatabase(url.DatabaseName);
