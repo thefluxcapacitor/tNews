c0eb947cb4d6b3b991148c17e01fd40f27b50906
thefluxcapacitor <thefluxcapacitor@thefluxcapacitor.com>
2013-09-02 17:57:02 -0300

    send json by default

--- TorrentNews/Global.asax.cs
+++ TorrentNews/Global.asax.cs
@@ -9,6 +9,10 @@ using System.Web.Routing;
 
 namespace TorrentNews
 {
+    using System.Net.Http.Headers;
+
+    using Newtonsoft.Json.Converters;
+
     using TorrentNews.App_Start;
 
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
@@ -24,6 +28,16 @@ namespace TorrentNews
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ConfigureWebApiFormatters();
         }
+
+        private static void ConfigureWebApiFormatters()
+        {
+            var jsonFormatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
+            jsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            jsonFormatter.SerializerSettings.Converters.Add(new IsoDateTimeConverter { DateTimeFormat = "yyyy-MM-dd HH:mm:ss" });
+        }
+
     }
 }
