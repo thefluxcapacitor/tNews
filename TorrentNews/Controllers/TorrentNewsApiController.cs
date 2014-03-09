a6604d264337a61332f9aa8c8701380824d6f831
thefluxcapacitor <thefluxcapacitor@thefluxcapacitor.com>
2014-03-09 19:54:48 -0300

    api for getting best release source

---
+++ TorrentNews/Controllers/TorrentNewsApiController.cs
@@ -0,0 +1,54 @@
+﻿namespace TorrentNews.Controllers
+{
+    using System.Net;
+    using System.Net.Http;
+    using System.Web;
+    using System.Web.Http;
+
+    using CsQuery;
+
+    using TorrentNews.Domain;
+
+    public class TorrentNewsApiController : ApiController
+    {
+        public ReleaseSource GetBestReleaseSource(string title, int year)
+        {
+            var bestRelease = new ReleaseSource() { Quality = -1 };
+
+            var client = new HttpClient(new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            });
+
+            var url = string.Format(
+                "http://kickass.to/usearch/{0}%20{1}%20category%3Amovies/?field=seeders&sorder=desc",
+                HttpUtility.UrlEncode(title),
+                year);
+
+            var response = client.GetAsync(url).Result;
+
+            CQ document = response.Content.ReadAsStringAsync().Result;
+            var rows = document[".data tr"];
+
+            foreach (var row in rows)
+            {
+                var rowCq = row.Cq();
+                if (rowCq.HasClass("firstr"))
+                {
+                    continue;
+                }
+
+                var torrentLink = rowCq.Find("div.torrentname > a.normalgrey");
+                var torrentTitle = torrentLink.Text();
+
+                var releaseSource = TorrentHelper.GetReleaseSource(torrentTitle);
+                if (releaseSource.Quality > bestRelease.Quality)
+                {
+                    bestRelease = releaseSource;
+                }
+            }
+
+            return bestRelease;
+        }
+    }
+}
---
+++ TorrentNews/Domain/TorrentHelper.cs
@@ -0,0 +1,87 @@
+﻿namespace TorrentNews.Domain
+{
+    using System.Text.RegularExpressions;
+
+    public class TorrentHelper
+    {
+        public static ReleaseSource GetReleaseSource(string torrentTitle)
+        {
+            // don't change order, always put first worst quality and last best quality
+
+            if (ReleaseFormatIs("cam|hdcam|camrip", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "Cam", Quality = 1 };
+            }
+
+            if (ReleaseFormatIs("ts|telesync|pdvd", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "Telesync", Quality = 1 };
+            }
+
+            if (ReleaseFormatIs("wp|workprint", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "Workprint", Quality = 1 };
+            }
+
+            if (ReleaseFormatIs("tc|telecine", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "Telecine", Quality = 1 };
+            }
+
+            if (ReleaseFormatIs("ppv|ppvrip", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "Pay-per-view", Quality = 2 };
+            }
+
+            if (ReleaseFormatIs("scr|screener|dvdscr|dvdscreener|bdscr|ddc", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "Screener", Quality = 2 };
+            }
+
+            if (ReleaseFormatIs(@"r5\.ac3\.5\.1\.hq|r5\.line|r5", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "R5", Quality = 2 };
+            }
+
+            if (ReleaseFormatIs(@"web\-rip|webrip|web rip", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "WEB Rip", Quality = 2 };
+            }
+
+            if (ReleaseFormatIs("dvdrip", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "DVDRip", Quality = 3 };
+            }
+
+            if (ReleaseFormatIs("dsr|dsrip|dthrip|dvbrip|hdtv|pdtv|tvrip|hdtvrip", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "HDTV", Quality = 3 };
+            }
+
+            if (ReleaseFormatIs("vodrip|vodr", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "VODRip", Quality = 3 };
+            }
+
+            if (ReleaseFormatIs(@"hdrip|bdrip|brrip|blu\-ray|bluray|bdr|bd5|bd9", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "BD/BRRip", Quality = 3 };
+            }
+
+            if (ReleaseFormatIs(@"webdl|web dl|web\-dl", torrentTitle))
+            {
+                return new ReleaseSource { DisplayValue = "WEB-DL", Quality = 3 };
+            }
+
+            return new ReleaseSource { DisplayValue = "Unknown", Quality = 0 };
+        }
+
+        private static bool ReleaseFormatIs(string formats, string torrentTitle)
+        {
+            var title = torrentTitle.ToLowerInvariant();
+            var regex = new Regex(@"[\.,\-,\s](" + formats + @")($|[\.,\-,\s])");
+            var match = regex.Match(title);
+            return match.Success;
+        }
+    }
+}
